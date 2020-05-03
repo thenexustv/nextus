c950d5e0700cd18bf8b141247049c5b9487268d0
Brian Mitchell <bman4789@gmail.com>
2020-05-03 01:48:29 -0500

    Oh so much prettier

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,28 +1,28 @@
 (() => {
-    function setup_navbar() {
-        const btn = document.getElementById('navbar-mobile-menu-toggle-button');
-        
-        const btnBurger = document.getElementById('navbar-mobile-menu-toggle-burger');
-        const btnEks = document.getElementById('navbar-mobile-menu-toggle-x');
-        
-        const menu = document.getElementById('navbar-mobile-menu');
-        btn.addEventListener('click', () => {
-            menu.classList.toggle('hidden');
-            menu.classList.toggle('block');
+  function setup_navbar() {
+    const btn = document.getElementById('navbar-mobile-menu-toggle-button');
 
-            btnBurger.classList.toggle('hidden');
-            btnBurger.classList.toggle('block');
+    const btnBurger = document.getElementById(
+      'navbar-mobile-menu-toggle-burger'
+    );
+    const btnEks = document.getElementById('navbar-mobile-menu-toggle-x');
 
-            btnEks.classList.toggle('hidden');
-            btnEks.classList.toggle('block');
-        });
+    const menu = document.getElementById('navbar-mobile-menu');
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+      menu.classList.toggle('block');
 
-    
-    }
+      btnBurger.classList.toggle('hidden');
+      btnBurger.classList.toggle('block');
 
-    function setup() {
-        setup_navbar();
-    }
-    
-    window.onload = setup
+      btnEks.classList.toggle('hidden');
+      btnEks.classList.toggle('block');
+    });
+  }
+
+  function setup() {
+    setup_navbar();
+  }
+
+  window.onload = setup;
 })();
